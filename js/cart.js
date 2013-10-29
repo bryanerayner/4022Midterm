59efbeb8152bf3a3a362dfa6313876344f80e6d0
bryanerayner <bryanerayner@Bryans-MacBook-Pro.local>
2013-10-29 00:23:36 -0400

    Add Marionette, Handlebars, Products JSON

--- js/cart.js
+++ js/cart.js
@@ -28,7 +28,6 @@ var Product = Backbone.Model.extend({
 
 	initialize:function()
 	{
-
 		this.on("addToCart", this.addToCart, this);
 	},
 
@@ -67,23 +66,95 @@ var inventory = new Inventory();
 
 inventory.reset([
 	{
-		colors:["green", "blue"],
+		colors:["black"],
 		brandName:"Adidas",
-		name:"Adidas F50 Adizero Cleat",
-		imgSrc:"images/image.jpg",
+		gender: "Men",
+		name:"Adidas 11 Pro Adipure Cleat",
+		imgSrc:"images/products/adidas_mens_adipure_11Pro_trx_fg_cleats.jpg",
 		price: 123,
-		promotionPrice: 12
+		promotionPrice: 12.12
 	},
 	{
-		colors:["green", "blue"],
+		colors:["red", "blue"],
 		brandName:"Adidas",
-		name:"Adidas F50 Adizero Cleat",
-		imgSrc:"images/image.jpg",
+		gender: "Men",
+		name:"Adidas F30 Messi TRX Cleat",
+		imgSrc:"images/products/adidas_mens_f30_trx_fg_Messi.jpg",
 		price: 123,
-		promotionPrice: 12
+		promotionPrice: 12.12
+	},
+	{
+		colors:["purple", "pink", "blue"],
+		gender: "Men",
+		brandName:"Adidas",
+		name:"Adidas F50 Adizero TRX Cleat",
+		imgSrc:"images/products/adidas_mens_f50_adizero_trx_fg_cleats.jpg",
+		price: 123,
+		promotionPrice: 12.12
+	},
+	{
+		colors:["black", "green"],
+		gender: "Men",
+		brandName:"Adidas",
+		name:"Adidas Predator Absolado LZ Cleat",
+		imgSrc:"images/products/adidas_mens_predator_absolado_lz_trx_fg_soccer_cleats_green.jpg",
+		price: 123,
+		promotionPrice: 12.12
+	},
+	{
+		colors:["black", "yellow"],
+		gender: "Men",
+		brandName:"Adidas",
+		name:"Adidas Predator Absolado TRX FG Cleat",
+		imgSrc:"images/products/adidas_mens_predator_absolado_lz_trx_fg_soccer_cleats_yellow.jpg",
+		price: 123,
+		promotionPrice: 12.12
+	},
+	{
+		colors:["purple", "white"],
+		gender: "Women",
+		brandName:"Adidas",
+		name:"Adidas F5 TRX FG Cleat",
+		imgSrc:"images/products/adidas_womens_f5_trx_fg_soccer_cleat.jpg",
+		price: 123,
+		promotionPrice: 12.12
+	},
+	{
+		colors:["black", "white"],
+		gender: "Men",
+		brandName:"New Balance",
+		name:"New Balance 5464 Cleat",
+		imgSrc:"images/products/new_balance_5464_lacrosse_cleats_men.jpg",
+		price: 123,
+		promotionPrice: 12.12
+	},
+	{
+		colors:["gray", "white"],
+		gender: "Men",
+		brandName:"New Balance",
+		name:"New Balance MB4040 Cleat",
+		imgSrc:"images/products/new_balance_mb4040_d_width_low_cut_baseball_cleats_men.jpg",
+		price: 123,
+		promotionPrice: 12.12
+	},
+	{
+		colors:["orange", "black"],
+		gender: "Men",
+		brandName:"Nike",
+		name:"Nike Hypervenom Phelon Cleat",
+		imgSrc:"images/products/nike_hypervenom_phelon_fg_outdoor_soccer_cleats_mens.jpg",
+		price: 123,
+		promotionPrice: 12.12
+	},
+	{
+		colors:["orange", "black"],
+		gender: "Men",
+		brandName:"Nike",
+		name:"Nike Mercurial Victory II Cleat",
+		imgSrc:"images/products/nike_mercurial_victory_II_fg_soccer_cleats_mens.jpg",
+		price: 123,
+		promotionPrice: 12.12
 	}
-
-
 	]);
 
 console.log(inventory);
